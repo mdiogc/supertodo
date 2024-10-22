7886a2c87d93d89b541e0fe86adc982969246fbd
alu <mdiogcasko@gmail.com>
2024-10-22 16:01:40 +0100

    weno

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'tasks'  # Namespaces the URLs for this app
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),
-    path('add/', views.add_task, name='add_task'),
-    path('<slug:task_slug>/', views.task_detail, name='task_detail'),
-    path('<slug:task_slug>/edit/', views.edit_task, name='edit_task'),
-    path('<slug:task_slug>/delete/', views.delete_task, name='delete_task'),
-    path('<slug:task_slug>/toggle/', views.toggle_task, name='toggle_task'),
+    path('', views.task_list, name='task-list'),
+    path('add/', views.add_task, name='add-task'),
+    path('<slug:task_slug>/', views.task_detail, name='task-detail'),
+    path('<slug:task_slug>/edit/', views.edit_task, name='edit-task'),
+    path('<slug:task_slug>/delete/', views.delete_task, name='delete-task'),
+    path('<slug:task_slug>/toggle/', views.toggle_task, name='toggle-task'),
 ]
